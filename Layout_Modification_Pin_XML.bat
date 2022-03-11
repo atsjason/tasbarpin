7a2503fdc42d484a8c48f8c96a7db34dd9a12905
atsjason <98031074+atsjason@users.noreply.github.com>
2022-03-11 12:45:47 -0500

    Update Layout_Modification_Pin_XML.bat

--- Layout_Modification_Pin_XML.bat
+++ Layout_Modification_Pin_XML.bat
@@ -1,7 +1,12 @@
 SET "LAYOUT_MODIFICATION_PATH=%localappdata%\Microsoft\Windows\Shell\LayoutModification.xml"
-SET "LAYOUT_MODIFICATION_URL=https://raw.githubusercontent.com/atsjason/tasbarpin/main/LayoutModification.xml"
+SET "SHORTCUT_CREATOR=%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
+
+IF EXIST "%SHORTCUT_CREATOR%" DEL /F "%SHORTCUT_CREATOR%"
+
+IF NOT EXIST "%SHORTCUT_CREATOR%" curl https://raw.githubusercontent.com/atsjason/taskbarUSB/main/Somatus_Google_Shortcuts.ps1 -o %userprofile%\Downloads\Somatus_Google_Shortcuts.ps1
+
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
 
 IF EXIST "%LAYOUT_MODIFICATION_PATH%" DEL /F "%LAYOUT_MODIFICATION_PATH%"
-REM IF NOT EXIST "%LAYOUT_MODIFICATION_PATH%" CURL %LAYOUT_MODIFICATION_URL% -O "%LAYOUT_MODIFICATION_PATH%"
 
-IF NOT EXIST "%LAYOUT_MODIFICATION_PATH%" curl https://raw.githubusercontent.com/atsjason/tasbarpin/main/LayoutModification.xml -o %localappdata%\Microsoft\Windows\Shell\LayoutModification.xml
+IF NOT EXIST "%LAYOUT_MODIFICATION_PATH%" copy C:\Shortcuts\LayoutModification.xml %localappdata%\Microsoft\Windows\Shell
