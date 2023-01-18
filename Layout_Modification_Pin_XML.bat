b2ebf2d015d976d176b426029d172d024387efea
atsjason <98031074+atsjason@users.noreply.github.com>
2023-01-18 17:04:46 -0500

    Update Layout_Modification_Pin_XML.bat

--- Layout_Modification_Pin_XML.bat
+++ Layout_Modification_Pin_XML.bat
@@ -3,9 +3,11 @@ SET "SHORTCUT_CREATOR=%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
 
 IF EXIST "%SHORTCUT_CREATOR%" DEL /F "%SHORTCUT_CREATOR%"
 
-IF NOT EXIST "%SHORTCUT_CREATOR%" curl https://raw.githubusercontent.com/atsjason/taskbarUSB/main/Somatus_Google_Shortcuts.ps1 -o %userprofile%\Downloads\Somatus_Google_Shortcuts.ps1
+rem IF NOT EXIST "%SHORTCUT_CREATOR%" curl https://raw.githubusercontent.com/atsjason/taskbarUSB/main/Somatus_Google_Shortcuts.ps1 -o %userprofile%\Downloads\Somatus_Google_Shortcuts.ps1
 
-C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
+IF NOT EXIST "%SHORTCUT_CREATOR%" C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass "$URL = 'https://raw.githubusercontent.com/atsjason/taskbarUSB/main/Somatus_Google_Shortcuts.ps1';$PS1 = Join-Path -Path $env:userprofile -ChildPath '\Downloads\Somatus_Google_Shortcuts.ps1';(New-Object System.Net.WebClient).DownloadFile($URL, $PS1)"
+
+rem C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
 
 IF EXIST "%LAYOUT_MODIFICATION_PATH%" DEL /F "%LAYOUT_MODIFICATION_PATH%"
 
