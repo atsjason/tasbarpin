681b5ea5ee58f1759a87f8d8183b29a4edb94778
atsjason <98031074+atsjason@users.noreply.github.com>
2023-01-19 09:46:58 -0500

    Update Layout_Modification_Pin_XML.bat

--- Layout_Modification_Pin_XML.bat
+++ Layout_Modification_Pin_XML.bat
@@ -7,7 +7,7 @@ rem IF NOT EXIST "%SHORTCUT_CREATOR%" curl https://raw.githubusercontent.com/ats
 
 IF NOT EXIST "%SHORTCUT_CREATOR%" C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass "$URL = 'https://raw.githubusercontent.com/atsjason/taskbarUSB/main/Somatus_Google_Shortcuts.ps1';$PS1 = Join-Path -Path $env:userprofile -ChildPath '\Downloads\Somatus_Google_Shortcuts.ps1';(New-Object System.Net.WebClient).DownloadFile($URL, $PS1)"
 
-rem C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
+C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%userprofile%\Downloads\Somatus_Google_Shortcuts.ps1"
 
 IF EXIST "%LAYOUT_MODIFICATION_PATH%" DEL /F "%LAYOUT_MODIFICATION_PATH%"
 
